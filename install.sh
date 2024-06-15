6cbc2979e8713f8880b2feb562d39ad5f7eb7c2a
RFS <7137874+rfs85@users.noreply.github.com>
2024-06-15 03:41:20 +0100

    nn

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # Function to install common dependencies
 function install_common_deps() {
   sudo apt update -y
-  sudo apt install -y curl wget
+  sudo apt install -y curl wget docker docker-compose
 }
 
 
