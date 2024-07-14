4351fb9c7bf34218a0844207172473e335f27137
RFS <7137874+rfs85@users.noreply.github.com>
2024-07-14 14:18:40 +0100

    ff

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Social Engineer LAB
+# Author: RFS 
+# Web: https://social-engineering.redlabs.cloud/
+# Discord: 
+# OS: Debian/Ubuntu/Kali
+# Version: 0.1
+
 
 
 
 # Function to install common dependencies
 function install_common_deps() {
   sudo apt update -y
-  sudo apt install -y curl wget docker docker-compose
+  sudo apt install -y curl git unzip wget docker docker-compose
 }
 
 
