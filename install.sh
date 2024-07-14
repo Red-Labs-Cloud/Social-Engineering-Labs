a8bc58d3839df25f4accc756e26aa10771e3a0c6
RFS <7137874+rfs85@users.noreply.github.com>
2024-07-14 15:09:39 +0100

    ll

--- install.sh
+++ install.sh
@@ -56,18 +56,18 @@ banner()
 lab()
 {
 
-echo -e "${RED} --------------------------------------------------------${ENDCOLOR}";
-echo -e "${YELLOW} | Frameworks/Apps   |           Access                 |  Functions           |${ENDCOLOR}";
+echo -e "${RED} ----------------------------------------------------------------------------------${ENDCOLOR}";
+echo -e "${YELLOW} | Frameworks/Apps   |           Access                 |      Functions       |${ENDCOLOR}";
 echo -e "${GREEN}  | GoPhish           | http://gophish.localhost:3000    |  Phishing campaigns  |${ENDCOLOR}";
 echo -e "${GREEN}  | EvilGinx          | http://evilginx.localhost:3000   |  MFA                 |${ENDCOLOR}";
 echo -e "${GREEN}  | Modlishka         | http://modlishka.localhost:3000  |  Description         |${ENDCOLOR}";
 echo -e "${GREEN}  | Muraena           | http://muraena.localhost:3000    |  Description         |${ENDCOLOR}";
 echo -e "${GREEN}  | EvilnoVNC         | http://evilnovnc.localhost:3000  |  Description         |${ENDCOLOR}";
-echo -e "${RED} --------------------------------------------------------${ENDCOLOR}";
-echo -e "${YELLOW} | Dashboards     |     URL  |      Technology         |${ENDCOLOR}";
-echo -e "${GREEN}  | Traefik   | http://10.10.11.5:5000/ |  Docker proxies           |${ENDCOLOR}";
-echo -e "${GREEN}  | PGAdmin   | http://10.10.11.5:5000/ |  Postgresql Dashboard             |${ENDCOLOR}";
-echo -e "${RED} --------------------------------------------------------${ENDCOLOR}";
+echo -e "${RED} ----------------------------------------------------------|----------------------|${ENDCOLOR}";
+echo -e "${YELLOW} | Dashboards   |     URL                 |         Technology                 |${ENDCOLOR}";
+echo -e "${GREEN}  | Traefik      | http://10.10.11.5:5000/ |  Docker proxies                    |${ENDCOLOR}";
+echo -e "${GREEN}  | PGAdmin      | http://10.10.11.5:5000/ |  Postgresql Dashboard              |${ENDCOLOR}";
+echo -e "${RED} ---------------------------------------------------------------------------------|${ENDCOLOR}";
 
 
 }
